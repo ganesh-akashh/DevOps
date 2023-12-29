ab682fd7647275a968eed3aa5cb2de7d0bf17c30
Akash G <ganeshakas2004@gmail.com>
2023-12-29 10:35:01 +0530

    docker

---
+++ my-app/app/home/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='h-screen grid place-content-center'>
+            <h1 className='text-2xl'>
+                Welcome to Docker 😎
+            </h1>
+        </div>
+    )
+}
+
+export default page
---
+++ my-app/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-white h-screen grid place-items-center'>
+      <h1 className='font-semibold text-2xl'>
+        Hello World 👋🏻 !!
+      </h1>
+    </div>
+  )
+}
+
+export default page
